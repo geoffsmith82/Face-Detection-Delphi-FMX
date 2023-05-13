a70e72e81318d12f1390bbce822bfbc4e3a94afb
Geoffrey Smith <fork@tysontechnology.com.au>
2023-05-13 18:56:49 +1000

    Refactor Face Detection Demo

--- examples/windows/face-detect/ufrmFaceDetect.pas
+++ examples/windows/face-detect/ufrmFaceDetect.pas
@@ -28,59 +28,8 @@ uses
   TensorFlowLiteFMX
   ;
 
-type
-  TfrmFaceDetect = class(TForm)
-    ImageMain: TImage;
-    btnOpenImage: TButton;
-    btnDetectFaces: TButton;
-    OpenDialog: TOpenDialog;
-    ImageList: TImageList;
-    cboProbability: TComboBox;
-    Label1: TLabel;
-    cboThreadCount: TComboBox;
-    Label2: TLabel;
-    cboInputSize: TComboBox;
-    Label3: TLabel;
-    Label4: TLabel;
-    Label5: TLabel;
-    edtBatchSize: TEdit;
-    procedure FormCreate(Sender: TObject);
-    procedure FormClose(Sender: TObject; var Action: TCloseAction);
-    procedure btnDetectFacesClick(Sender: TObject);
-    procedure btnOpenImageClick(Sender: TObject);
-    procedure cboThreadCountChange(Sender: TObject);
-    procedure cboInputSizeChange(Sender: TObject);
-  private
-    FFaceDetection: TTensorFlowLiteFMX;
-  public
-    procedure LoadImage(inFilename: string);
-    procedure ReloadModel;
-  end;
-
-var
-  frmFaceDetect: TfrmFaceDetect;
-
-implementation
-
-{$R *.fmx}
-
-const
-  ModelsPath = '..\..\..\..\..\models\';
 
 
-  // AMD Ryzen 5 3500X, Windows 10, 8 threads, CPU
-var
-  // tflite models with static input shape
-  // 160, 1575 - image with 160x160 pixels, inference time 0.009 sec, 110 frame per sec, good detection quality, perfect for selfies
-  // 192, 2268
-  // 256, 4032
-  // 320, 6300 - image with 320x320 pixels, inference time 0.031 sec, 32 frame per sec, high detection quality
-  // 480, 14175 - image with 480x480 pixels, inference time 0.064 sec, 15 frame per sec, high detection quality
-  // 640, 25200 - image with 640x640 pixels, inference time 0.109 sec, 9 frame per sec, high quality detection
-  // 800, 39375 - image with 800x800pixels, inference time 0.109 sec, 9 frame per sec, high quality detection
-
-  FaceDetectionInputSize: Int32 = 640;
-  FaceDetectionOutputSize: Int32 = 25200;
 
 type
   PInputDataFaceDetection = ^TInputDataFaceDetection;
@@ -102,22 +51,88 @@ type
     Count: Int32;
   end;
 
+type
+  TFaceDetect = class
+  private
+    FFaceDetection: TTensorFlowLiteFMX;
+
+    // AMD Ryzen 5 3500X, Windows 10, 8 threads, CPU
+    // tflite models with static input shape
+    // 160, 1575 - image with 160x160 pixels, inference time 0.009 sec, 110 frame per sec, good detection quality, perfect for selfies
+    // 192, 2268
+    // 256, 4032
+    // 320, 6300 - image with 320x320 pixels, inference time 0.031 sec, 32 frame per sec, high detection quality
+    // 480, 14175 - image with 480x480 pixels, inference time 0.064 sec, 15 frame per sec, high detection quality
+    // 640, 25200 - image with 640x640 pixels, inference time 0.109 sec, 9 frame per sec, high quality detection
+    // 800, 39375 - image with 800x800pixels, inference time 0.109 sec, 9 frame per sec, high quality detection
+
+    FaceDetectionInputSize: Int32;
+    FaceDetectionOutputSize: Int32;
+    procedure LoadImage(inFilename: string);
+  public
+    ImageList: TImageList;
+    ThreadCount : Integer;
+    BatchSize: Int32;
+    procedure DetectFaces(inFilename: string);
+    function LoadModel(ModelPath: String; InterpreterThreadCount: Integer): TFLiteStatus;
+    function GetFaceList(Probability: Float32; NMS: Integer; OutputData: POutputDataFaceDetection): TFaceList;
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+  TfrmFaceDetect = class(TForm)
+    ImageMain: TImage;
+    btnOpenImage: TButton;
+    btnDetectFaces: TButton;
+    OpenDialog: TOpenDialog;
+    ImageList2: TImageList;
+    cboProbability: TComboBox;
+    Label1: TLabel;
+    cboThreadCount: TComboBox;
+    Label2: TLabel;
+    cboInputSize: TComboBox;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    edtBatchSize: TEdit;
+    procedure FormCreate(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+    procedure btnDetectFacesClick(Sender: TObject);
+    procedure btnOpenImageClick(Sender: TObject);
+    procedure cboThreadCountChange(Sender: TObject);
+    procedure cboInputSizeChange(Sender: TObject);
+  private
+    FaceDetect : TFaceDetect;
+  public
+    procedure ReloadModel;
+  end;
+
+var
+  frmFaceDetect: TfrmFaceDetect;
+
+implementation
+
+{$R *.fmx}
+
+const
+  ModelsPath = '..\..\..\..\..\models\';
+
+
 var
   HideProbability: Boolean = False;
-  BatchSize: Int32 = 1;
 
-procedure TfrmFaceDetect.LoadImage(inFilename: string);
+procedure TFaceDetect.LoadImage(inFilename: string);
 begin
-  Label4.Text := '';
+  frmFaceDetect.Label4.Text := '';
 
 {$IFDEF MSWINDOWS}
   if not FileExists(inFilename) then
     Exit;
 
-  if ImageMain.MultiResBitmap.Count > 0 then
-    ImageMain.MultiResBitmap[0].Free;
+  if frmFaceDetect.ImageMain.MultiResBitmap.Count > 0 then
+    frmFaceDetect.ImageMain.MultiResBitmap[0].Free;
 
-  ImageMain.MultiResBitmap.Add;
+  frmFaceDetect.ImageMain.MultiResBitmap.Add;
 
   if FileExists(inFilename) then
   begin
@@ -165,7 +180,7 @@ begin
     ImageList.Source[1].MultiResBitmap[0].Bitmap.Canvas.EndScene;
   end;
 
-  ImageMain.Bitmap.Assign(ImageList.Source[1].MultiResBitmap[0].Bitmap);
+  frmFaceDetect.ImageMain.Bitmap.Assign(ImageList.Source[1].MultiResBitmap[0].Bitmap);
 
 {$ENDIF MSWINDOWS}
 end;
@@ -173,10 +188,10 @@ end;
 procedure TfrmFaceDetect.btnOpenImageClick(Sender: TObject);
 begin
   if OpenDialog.Execute then
-    LoadImage(OpenDialog.FileName);
+    FaceDetect.LoadImage(OpenDialog.FileName);
 end;
 
-function GetFaceList(Probability: Float32; NMS: Integer; OutputData: POutputDataFaceDetection): TFaceList;
+function TFaceDetect.GetFaceList(Probability: Float32; NMS: Integer; OutputData: POutputDataFaceDetection): TFaceList;
 var
   i, X, Y: DWORD;
   LListNMS: array of TFace;
@@ -292,6 +307,13 @@ begin
 end;
 
 procedure TfrmFaceDetect.btnDetectFacesClick(Sender: TObject);
+begin
+  FaceDetect.BatchSize := StrToIntDef(edtBatchSize.Text, 1);
+  FaceDetect.DetectFaces(OpenDialog.FileName);
+end;
+
+
+procedure TFaceDetect.DetectFaces(inFilename: string);
 var
   LBatch, i, X, Y, LPixel: DWORD;
   LColors: PAlphaColorArray;
@@ -303,9 +325,8 @@ var
   LRect: TRectF;
   LTickCountInference, LTickCountNMS: Int64;
 begin
-  BatchSize := StrToIntDef(edtBatchSize.Text, 1);
 
-  LoadImage(OpenDialog.FileName);
+  LoadImage(inFilename);
 
   if ImageList.Source[1].MultiResBitmap.Count = 0 then
     Exit;
@@ -363,38 +384,38 @@ begin
 
           LTickCountNMS := TThread.GetTickCount64;
 
-          LFaceList := GetFaceList(StrToFloat(cboProbability.Items[cboProbability.ItemIndex]), 10, LOutputData);
+          LFaceList := GetFaceList(StrToFloat(frmFaceDetect.cboProbability.Items[frmFaceDetect.cboProbability.ItemIndex]), 10, LOutputData);
 
           if LBatch = BatchSize - 1 then
           begin
-            ImageMain.Bitmap.Canvas.BeginScene;
+            frmFaceDetect.ImageMain.Bitmap.Canvas.BeginScene;
             try
               LRect.Width := Screen.Width;
               LRect.Height := Screen.Height;
 
               if LFaceList.Count > 0 then
               begin
-                Label4.Text := 'detect time: ' + FloatToStr((TThread.GetTickCount64 - LTickCountInference) / 1000 / BatchSize) + ', nms time: ' + FloatToStr((TThread.GetTickCount64 - LTickCountNMS) / 1000) + ', face count: ' + IntToStr(LFaceList.Count);
+                frmFaceDetect.Label4.Text := 'detect time: ' + FloatToStr((TThread.GetTickCount64 - LTickCountInference) / 1000 / BatchSize) + ', nms time: ' + FloatToStr((TThread.GetTickCount64 - LTickCountNMS) / 1000) + ', face count: ' + IntToStr(LFaceList.Count);
 
-                ImageMain.Bitmap.Canvas.Font.Size := 11;
-                ImageMain.Bitmap.Canvas.MeasureText(LRect, '0,00', False, [], TTextAlign.Leading, TTextAlign.Leading);
-                ImageMain.Bitmap.Canvas.Stroke.Color := TAlphaColorRec.Orangered;
-                ImageMain.Bitmap.Canvas.Stroke.Thickness := 1.5;
+                frmFaceDetect.ImageMain.Bitmap.Canvas.Font.Size := 11;
+                frmFaceDetect.ImageMain.Bitmap.Canvas.MeasureText(LRect, '0,00', False, [], TTextAlign.Leading, TTextAlign.Leading);
+                frmFaceDetect.ImageMain.Bitmap.Canvas.Stroke.Color := TAlphaColorRec.Orangered;
+                frmFaceDetect.ImageMain.Bitmap.Canvas.Stroke.Thickness := 1.5;
 
                 for i := 0 to LFaceList.Count - 1 do
                 begin
-                  ImageMain.Bitmap.Canvas.DrawRect(LFaceList.Faces[i].Rect, 0, 0, AllCorners, 1);
+                  frmFaceDetect.ImageMain.Bitmap.Canvas.DrawRect(LFaceList.Faces[i].Rect, 0, 0, AllCorners, 1);
 
-                  ImageMain.Bitmap.Canvas.Fill.Color := TAlphaColorRec.White;
+                  frmFaceDetect.ImageMain.Bitmap.Canvas.Fill.Color := TAlphaColorRec.White;
                   if not HideProbability then
-                    ImageMain.Bitmap.Canvas.FillText(
+                    frmFaceDetect.ImageMain.Bitmap.Canvas.FillText(
                       RectF(LFaceList.Faces[i].Rect.Left, LFaceList.Faces[i].Rect.Top - LRect.Height, LFaceList.Faces[i].Rect.Right + LRect.Width, LFaceList.Faces[i].Rect.Bottom),
                       Copy(FloatToStr(LFaceList.Faces[i].Probability), 1, 4),
                       False, 1, [], TTextAlign.Leading, TTextAlign.Leading);
                 end;
               end;
             finally
-              ImageMain.Bitmap.Canvas.EndScene;
+              frmFaceDetect.ImageMain.Bitmap.Canvas.EndScene;
             end;
           end;
 
@@ -414,45 +435,45 @@ begin
   case cboInputSize.ItemIndex of
     0:
       begin
-        FaceDetectionInputSize := 160;
-        FaceDetectionOutputSize := 1575;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_160.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 160;
+        FaceDetect.FaceDetectionOutputSize := 1575;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_160.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     1:
       begin
-        FaceDetectionInputSize := 192;
-        FaceDetectionOutputSize := 2268;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_192.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 192;
+        FaceDetect.FaceDetectionOutputSize := 2268;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_192.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     2:
       begin
-        FaceDetectionInputSize := 256;
-        FaceDetectionOutputSize := 4032;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_256.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 256;
+        FaceDetect.FaceDetectionOutputSize := 4032;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_256.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     3:
       begin
-        FaceDetectionInputSize := 320;
-        FaceDetectionOutputSize := 6300;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_320.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 320;
+        FaceDetect.FaceDetectionOutputSize := 6300;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_320.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     4:
       begin
-        FaceDetectionInputSize := 480;
-        FaceDetectionOutputSize := 14175;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_480.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 480;
+        FaceDetect.FaceDetectionOutputSize := 14175;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_480.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     5:
       begin
-        FaceDetectionInputSize := 640;
-        FaceDetectionOutputSize := 25200;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_640.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 640;
+        FaceDetect.FaceDetectionOutputSize := 25200;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_640.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
     6:
       begin
-        FaceDetectionInputSize := 800;
-        FaceDetectionOutputSize := 39375;
-        FFaceDetection.LoadModel(ModelsPath + 'face_detection_800.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
+        FaceDetect.FaceDetectionInputSize := 800;
+        FaceDetect.FaceDetectionOutputSize := 39375;
+        FaceDetect.LoadModel(ModelsPath + 'face_detection_800.tflite', StrToInt(cboThreadCount.Items[cboThreadCount.ItemIndex]));
       end;
   end;
 end;
@@ -471,18 +492,52 @@ procedure TfrmFaceDetect.FormCreate(Sender: TObject);
 begin
 {$IFDEF MSWINDOWS}
   SetPriorityClass(GetCurrentProcess, HIGH_PRIORITY_CLASS);
-
-  FFaceDetection := TTensorFlowLiteFMX.Create(Self);
-
-  // Currently Tensor Flow Lite for Windows supports only x64 CPU, GPU is not supported
-
-  FFaceDetection.LoadModel(ModelsPath + 'face_detection_640.tflite', 8);
+  FaceDetect := TFaceDetect.Create;
 {$ENDIF}
 end;
 
 procedure TfrmFaceDetect.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-  FFaceDetection.Destroy;
+  FreeAndNil(FaceDetect);
+end;
+
+{ TFaceDetect }
+
+constructor TFaceDetect.Create;
+var
+  LSourceItem : TCustomSourceItem;
+begin
+  FFaceDetection := TTensorFlowLiteFMX.Create(nil);
+  // Currently Tensor Flow Lite for Windows supports only x64 CPU, GPU is not supported
+  FaceDetectionInputSize :=640;
+  FaceDetectionOutputSize := 25200;
+  BatchSize := 1;
+  ImageList := TImageList.Create(nil);
+  LSourceItem := ImageList.Source.Add;
+  LSourceItem.Name := 'Source';
+
+  LSourceItem := ImageList.Source.Add;
+  LSourceItem.Name := 'FaceSegment';
+
+  LSourceItem := ImageList.Source.Add;
+  LSourceItem.Name := 'IrisLeft';
+
+  LSourceItem := ImageList.Source.Add;
+  LSourceItem.Name := 'IrisRight';
+
+  FFaceDetection.LoadModel(ModelsPath + 'face_detection_640.tflite', 8);
+end;
+
+destructor TFaceDetect.Destroy;
+begin
+  FreeAndNil(FFaceDetection);
+  FreeAndNil(ImageList);
+  inherited;
+end;
+
+function TFaceDetect.LoadModel(ModelPath: String; InterpreterThreadCount: Integer): TFLiteStatus;
+begin
+  Result := FFaceDetection.LoadModel(ModelPath, InterpreterThreadCount);
 end;
 
 end.
